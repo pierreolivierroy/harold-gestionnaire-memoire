228d98db136cddaeef6fef37d69f92df2a54c3fe
pierreolivierroy <roy.pierre.o@gmail.com>
2015-03-30 17:55:45 -0400

    Implémentation de toutes les fonctions de l'interface

--- src/FirstFitManager.h
+++ src/FirstFitManager.h
@@ -1,9 +1,47 @@
 class FirstFitManager : public IMemoryManager
 {
     public:
+    	virtual void initMem()
+    	{
+    		std::cout << "initMem\n";
+    	}
         virtual void memAlloc()
         {
             //do stuff
             std::cout << "memAlloc\n";
         }
+		virtual void freeMem(/*block*/)
+		{
+			std::cout << "freeMem\n";
+		}
+		virtual int getFreeBlockCount()
+		{
+			std::cout << "getFreeBlockCount\n";
+			return 0;
+		}
+		virtual int getBlockCount()
+		{
+			std::cout << "getBlockCount\n";
+			return 0;
+		}
+		virtual int getFreeMem()
+		{
+			std::cout << "getFreeMem\n";
+			return 0;
+		}
+		virtual int getBiggestFreeBlock()
+		{
+			std::cout << "getBiggestFreeBlock\n";
+			return 0;
+		}
+		virtual int getSmallFreeBlockCount(int)
+		{
+			std::cout << "getSmallFreeBlockCount\n";
+			return 0;
+		}
+		virtual bool isMemAlloc()
+		{
+			std::cout << "isMemAlloc\n";
+			return 0;
+		}
 };
